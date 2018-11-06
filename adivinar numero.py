396baaf4f237f350bc8d93ba431bc4dec1d1a5ed
Rodrigo <rodrigamtri@gmail.com>
2018-11-06 19:40:01 +0100

    añadiendo el archivo prueba

---
+++ adivinar numero.py
@@ -0,0 +1,44 @@
+
+
+number_to_guess = 2
+win = 0
+
+user_number = int(input("Introduce el numero: "))
+
+if number_to_guess == user_number:
+    print("Has ganado")
+    win = 1
+else:
+    print("Has perdido")
+
+if win == 0:
+    user_number = int(input("Introduce el numero: "))
+    if number_to_guess == user_number:
+        print("Has ganado")
+        win = 1
+    else:
+        print("Has perdido")
+
+if win == 0:
+    user_number = int(input("Introduce el numero: "))
+    if number_to_guess == user_number:
+        print("Has ganado")
+        win = 1
+    else:
+        print("Has perdido")
+
+if win == 0:
+    user_number = int(input("Introduce el numero: "))
+    if number_to_guess == user_number:
+        print("Has ganado")
+        win = 1
+    else:
+        print("Has perdido")
+
+if win == 0:
+    user_number = int(input("Introduce el numero: "))
+    if number_to_guess == user_number:
+        print("Has ganado")
+        win = 1
+    else:
+        print("Has perdido")

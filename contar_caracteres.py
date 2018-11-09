4159e937f9b163fce0b09a8c22473bd3616f2864
Rodrigo <rodrigamtri@gmail.com>
2018-11-09 17:44:11 +0100

    Este programa es capaz de contar el numero de carcteres que hay en texto

--- contar_caracteres.py
+++ contar_caracteres.py
@@ -3,18 +3,38 @@
 
 frase_usuario = input("Introduce, una frase: ")
 
-vocales = [a, e, i, o, u]
-consonantes = [b, c, d, f, g, h, j, k, l, m, n, p, q, r, s, t, v, w, x, y, z]
+vocales = ["a", "e", "i", "o", "u"]
+consonantes = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
 
 n_vocales = 0
 n_consonantes = 0
+n_espacios = 0
+n_puntos = 0
+n_comas = 0
+n_exclamaciones = 0
+n_interrogaciones = 0
 
 for letra in frase_usuario:
     if letra in vocales:
         n_vocales += 1
     elif letra in consonantes:
         n_consonantes += 1
+    elif letra == " ":
+        n_espacios += 1
+    elif letra == ",":
+        n_comas += 1
+    elif letra == "!" or "¡":
+        n_exclamaciones += 1
+    elif letra == ".":
+        n_puntos += 1
+    elif letra == "¿" or "?":
+        n_interrogaciones += 1
 
 
 print("Hay {} vocales".format(n_vocales))
 print("Hay {} consonantes".format(n_consonantes))
+print("Hay {} espacios".format(n_espacios))
+print("Hay {} puntos".format(n_puntos))
+print("Hay {} comas".format(n_comas))
+print("Hay {} excalamaciones".format(n_exclamaciones))
+print("Hay {} interrogaciones".format(n_interrogaciones))

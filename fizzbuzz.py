776f3ca4d76addfd1bb7f37d001122a448a27147
Rodrigo <rodrigamtri@gmail.com>
2018-11-10 11:15:41 +0100

    Ejercicios con for

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,10 @@
 
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 30, 60, 70]
-
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 90, 10, 11, 12, 14, 15, 30, 60, 70]
+numero_grande = numeros[0]
 
 for indice in range(len(numeros)):
-    if numeros[indice] % 3 == 0 and numeros[indice] % 5 == 0:
-        numeros[indice] = "Bazinga"
-    elif numeros[indice] % 3 == 0:
-        numeros[indice] = "Fizz"
-    elif numeros[indice] % 5 == 0:
-        numeros[indice] = "Buzz"
+    if numero_grande < numeros[indice]:
+        numero_grande = numeros[indice]
 
-print(numeros)
+
+print(numero_grande)

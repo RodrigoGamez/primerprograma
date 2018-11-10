e44a5f863a68b5566d388c57d23f6c3f423e4e37
Rodrigo <rodrigamtri@gmail.com>
2018-11-10 11:13:55 +0100

    Ejercicios con for

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,7 +4,7 @@ numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 30, 60, 70]
 
 for indice in range(len(numeros)):
     if numeros[indice] % 3 == 0 and numeros[indice] % 5 == 0:
-        numeros[indice] = "FizzBuzz"
+        numeros[indice] = "Bazinga"
     elif numeros[indice] % 3 == 0:
         numeros[indice] = "Fizz"
     elif numeros[indice] % 5 == 0:

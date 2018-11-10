40022737aaabc6896ab5515f08f92e5b7190038a
Rodrigo <rodrigamtri@gmail.com>
2018-11-10 10:46:54 +0100

    Ejercicios replace

--- format.py
+++ format.py
@@ -1,15 +1,26 @@
 #Haciendo un format con el metodo replace
 
 
-texto_usuario = input("Introduce un texto: ")
-valor_cambiar = "i"
+texto_cambiar = "aurora boreal"
+valor = 1
 
 
-for valor in texto_usuario:
-    texto_usuario = texto_usuario.replace("a", valor_cambiar, 1)
-    texto_usuario = texto_usuario.replace("e", valor_cambiar, 1)
-    texto_usuario = texto_usuario.replace("o", valor_cambiar, 1)
-    texto_usuario = texto_usuario.replace("u", valor_cambiar, 1)
+for dato in texto_cambiar:
+    if dato == "a":
+        texto_cambiar = texto_cambiar.replace("a", str(valor), 1)
+        valor += 1
+    elif dato == "e":
+        texto_cambiar = texto_cambiar.replace("e", str(valor), 1)
+        valor += 1
+    elif dato == "i":
+        texto_cambiar = texto_cambiar.replace("i", str(valor), 1)
+        valor += 1
+    elif dato == "o":
+        texto_cambiar = texto_cambiar.replace("o", str(valor), 1)
+        valor += 1
+    elif dato == "u":
+        texto_cambiar = texto_cambiar.replace("u", str(valor), 1)
+        valor += 1
 
 
-print(texto_usuario)
+print(texto_cambiar)

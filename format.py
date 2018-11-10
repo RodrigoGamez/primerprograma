c4872bd191705fe7a0a3fc33fd774e2e49be2390
Rodrigo <rodrigamtri@gmail.com>
2018-11-10 10:34:41 +0100

    Ejercicios replace

--- format.py
+++ format.py
@@ -2,13 +2,14 @@
 
 
 texto_usuario = input("Introduce un texto: ")
-valor_cambiar = "E"
+valor_cambiar = "i"
 
 
-for letra in texto_usuario:
-    if letra == "A":
-        texto_usuario = texto_usuario.replace("A", valor_cambiar, 1)
-    elif letra == "a":
-        texto_usuario = texto_usuario.replace("a", valor_cambiar, 1)
+for valor in texto_usuario:
+    texto_usuario = texto_usuario.replace("a", valor_cambiar, 1)
+    texto_usuario = texto_usuario.replace("e", valor_cambiar, 1)
+    texto_usuario = texto_usuario.replace("o", valor_cambiar, 1)
+    texto_usuario = texto_usuario.replace("u", valor_cambiar, 1)
+
 
 print(texto_usuario)

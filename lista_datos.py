d08ddbba3dcf8cae488a18f7b25f2dd00e581cd5
Rodrigo <rodrigamtri@gmail.com>
2018-11-10 10:07:34 +0100

    Ejercicios

--- lista_datos.py
+++ lista_datos.py
@@ -1,10 +1,14 @@
 
-
-lista_datos = [9, 9, 2]
-resultado = 1
+lista_datos = [3, 4, "Hola", "Telefono", 3, "Raton"]
+lista_enteros = []
+lista_string = []
 
 
 for dato in lista_datos:
-    resultado = resultado * dato
+    if type(dato) == int:
+        lista_enteros.append(dato)
+    elif type(dato) == str:
+        lista_string.append(dato)
 
-print(resultado)
+print("Enteros: {}".format(lista_enteros))
+print("Strings: {}".format(lista_string))

af684097e05d5b8e10f86a002c8c095ca9f698b2
Rodrigo <rodrigamtri@gmail.com>
2018-11-21 18:43:45 +0100

    creando archivos

--- time.py
+++ time.py
@@ -1,30 +1,10 @@
 
 
 from time import sleep
-import datetime
+from random import choice
 
-NIGHT_STARTS = 19
-DAY_STARTS = 8
-HOUR_DURATION = 1
+FRASES = ["pos ok pos vale pos me mato", "yo me llamo ralph", "el aliento de mi gato huele a comida de gato"]
 
-
-def main():
-    current_time = datetime.datetime.now()
-    while True:
-        sleep(HOUR_DURATION)
-        current_time += datetime.timedelta(hours=1)
-
-        if current_time.hour >= NIGHT_STARTS or current_time.hour <= DAY_STARTS:
-            day_or_night = "Noche"
-
-        else:
-            day_or_night = "Dia"
-
-        with open("horas.txt", "w") as hours_file:
-            time_text = "La hora actual es: {}, es de: {}\n".format(current_time, day_or_night)
-            hours_file.write(time_text)
-
-
-
-if __name__ == "__main__":
-    main()
+while True:
+    print(choice(FRASES))
+    sleep(3)

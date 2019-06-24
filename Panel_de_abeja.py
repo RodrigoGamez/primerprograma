cba07e2ecf41b6064e70c5f551808257bedc5bd7
Rodrigo <rodrigamtri@gmail.com>
2019-06-24 15:45:39 +0200

    Panel Abejas

--- Panel_de_abeja.py
+++ Panel_de_abeja.py
@@ -1,3 +1,5 @@
+import random
+
 columna1 = [0, 11, 0, False]
 columna2 = [0, 6, 1, 0, False]
 columna3 = [0, 8, 0, 7, 0, False]
@@ -11,21 +13,32 @@ fila4 = [columna2[3], columna3[3], columna4[2], columna5[1], False]
 fila5 = [columna3[4], columna4[3], columna5[2], False]
 
 
-def comprobar_suma(lista):
-    suma = 0
-    for numero in lista:
-        suma += numero
-    if suma == 38:
-        lista[-1] = True
-    return suma
+numeros = [0, 2, 3, 4, 5, 9, 10, 12, 14, 15, 16, 17, 18, 19]
 
 
-def mostrar_panal():
+def completar(lista):
+    total = 0
+    while total != 38:
+        for index in range(0, len(lista)-1):
+            if lista[index] in numeros:
+                lista[index] = random.choice(numeros)
+            total += lista[index]
+        print("{}           {}".format(lista, total))
+        if total == 38:
+            lista[-1] = True
+            break
+        total = 0
+
+
+def mostrar():
+    print("\n")
+
     print(columna1)
     print(columna2)
     print(columna3)
     print(columna4)
     print(columna5)
+
     print("\n")
 
     print(fila1)
@@ -33,21 +46,6 @@ def mostrar_panal():
     print(fila3)
     print(fila4)
     print(fila5)
-    print("\n")
-
-def sumar(lista):
-    for index in range(0, len(lista)-1):
-        lista[index] += 1
-
-
-def completar(lista, fila1, fila2, fila3, fila4, fila5):
-    while comprobar_suma(lista) < 38:
-        sumar(lista)
-    fila1 = [columna1[0], columna2[0], columna3[0]]
-    fila2 = [columna1[1], columna2[1], columna3[1], columna4[0]]
-    fila3 = [columna1[2], columna2[2], columna3[2], columna4[1], columna5[0]]
-    fila4 = [columna2[3], columna3[3], columna4[2], columna5[1]]
-    fila5 = [columna3[4], columna4[3], columna5[2]]
 
 
 def main():
@@ -56,7 +54,9 @@ def main():
     completar(columna3)
     completar(columna4)
     completar(columna5)
-    mostrar_panal()
+
+
+    mostrar()
 
 
 if __name__ == "__main__":
